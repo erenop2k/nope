fc38945180838439970085319f3bf2f4a9b5d024
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-03-07 23:54:25 +0530

    Update userinfo.py

--- SiestaRobot/modules/userinfo.py
+++ SiestaRobot/modules/userinfo.py
@@ -301,22 +301,22 @@ def info(update: Update, context: CallbackContext):
     disaster_level_present = False
 
     if user.id == OWNER_ID:
-        text += "\n\nThe Disaster level of this person is 'ʟᴜꜰꜰʏ'."
+        text += "\n\nThe Disaster level of this person is 'Darling'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThis user is member of '𝙰𝙷𝙹𝙸𝙽'."
+        text += "\n\nThis user is member of 'Oniichan'."
         disaster_level_present = True
     elif user.id in DRAGONS:
-        text += "\n\nThe Disaster level of this person is 'ɴᴇᴡᴡᴏʀʟᴅ'."
+        text += "\n\nThe Disaster level of this person is 'Sensei'."
         disaster_level_present = True
     elif user.id in DEMONS:
-        text += "\n\nThe Disaster level of this person is 'ʏᴏɴᴋᴏ'."
+        text += "\n\nThe Disaster level of this person is 'Senpai'."
         disaster_level_present = True
     elif user.id in TIGERS:
-        text += "\n\nThe Disaster level of this person is 'Captain'."
+        text += "\n\nThe Disaster level of this person is 'Bestfriend'."
         disaster_level_present = True
     elif user.id in WOLVES:
-        text += "\n\nThe Disaster level of this person is 'Soldier'."
+        text += "\n\nThe Disaster level of this person is 'Friend'."
         disaster_level_present = True
     elif user.id == 1829047705:
          text += "\n\nOwner Of A Bot. Queen Of @excrybaby. Bot Name Inspired From 'JoJo'."
@@ -346,25 +346,26 @@ def info(update: Update, context: CallbackContext):
     if INFOPIC:
         try:
             profile = context.bot.get_user_profile_photos(user.id).photos[0][-1]
-            context.bot.sendChatAction(chat.id, "upload_photo")
-            context.bot.send_photo(
-                chat.id,
-                photo=profile,
+            _file = bot.get_file(profile["file_id"])
+            _file.download(f"{user.id}.png")
+
+            message.reply_document(
+                document=open(f"{user.id}.png", "rb"),
                 caption=(text),
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton(
-                                "Health", url="https://t.me/darlinsz/99"),
+                                "Health", url="https://t.me/ShikimoriXupdates/11"),
                             InlineKeyboardButton(
-                                "Disaster", url="https://t.me/darlinsz/98")
+                                "Disaster", url="https://t.me/ShikimoriXupdates/9")
                         ],
                     ]
                 ),
                 parse_mode=ParseMode.HTML,
             )
 
-            os.remove(f"{user.id}.jpg")
+            os.remove(f"{user.id}.png")
         # Incase user don't have profile pic, send normal text
         except IndexError:
             message.reply_text(
@@ -373,9 +374,9 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "Health", url="https://t.me/darlinsz/99"),
+                                "Health", url="https://t.me/ShikimoriXupdates/11"),
                             InlineKeyboardButton(
-                                "Disaster", url="https://t.me/darlinsz/98")
+                                "Disaster", url="https://t.me/ShikimoriXupdates/9")
                         ],
                     ]
                 ),
@@ -446,50 +447,15 @@ def set_about_me(update: Update, context: CallbackContext):
             )
 
 @sudo_plus
-def stats(update, context):
-    uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
-    botuptime = get_readable_time((time.time() - StartTime))
-    status = "*╒═══「 System statistics 」*\n\n"
-    status += "*➢ System Start time:* " + str(uptime) + "\n"
-    uname = platform.uname()
-    status += "*➢ System:* " + str(uname.system) + "\n"
-    status += "*➢ Node name:* " + escape_markdown(str(uname.node)) + "\n"
-    status += "*➢ Release:* " + escape_markdown(str(uname.release)) + "\n"
-    status += "*➢ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
-    mem = virtual_memory()
-    cpu = cpu_percent()
-    disk = disk_usage("/")
-    status += "*➢ CPU:* " + str(cpu) + " %\n"
-    status += "*➢ RAM:* " + str(mem[2]) + " %\n"
-    status += "*➢ Storage:* " + str(disk[3]) + " %\n\n"
-    status += "*➢ Python Version:* " + python_version() + "\n"
-    status += "*➢ python-Telegram-Bot:* " + str(ptbver) + "\n"
-    status += "*➢ Uptime:* " + str(botuptime) + "\n"
-    try:
-        update.effective_message.reply_text(
-            status
-            + "\n*Bot statistics*:\n"
-            + "\n".join([mod.__stats__() for mod in STATS])
-            + f"\n\n[✦ Support](https://t.me/{SUPPORT_CHAT}) | [✦ Updates](https://t.me/darlinsz)\n\n"
-            + "╘══「 by [Light Yagami 夜神月](https://github.com/itz_light_yagami) 」\n",
-            parse_mode=ParseMode.MARKDOWN,
-            disable_web_page_preview=True,
-        )
-    except BaseException:
-        update.effective_message.reply_text(
-            (
-                (
-                    (
-                        "\n*Bot statistics*:\n"
-                        + "\n".join(mod.__stats__() for mod in STATS)
-                    )
-                    + f"\n\n✦ [Support](https://t.me/{SUPPORT_CHAT}) | ✦ [Updates](https://t.me/Black_Knights_Union)\n\n"
-                )
-                + "╘══「 by [Awesome-RJ](https://github.com/Awesome-RJ) 」\n"
-            ),
-            parse_mode=ParseMode.MARKDOWN,
-            disable_web_page_preview=True,
-        )
+def stats(update: Update, context: CallbackContext):
+    stats = "<b>╔═━「 Current Shikimor Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
+    result += "\n<b>╘═━「 Powered By Ahjin 」</b>"
+    update.effective_message.reply_text(
+        result,
+        parse_mode=ParseMode.HTML, 
+        disable_web_page_preview=True
+   )
         
         
 def about_bio(update: Update, context: CallbackContext):

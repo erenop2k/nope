a99ad700d8e3b8051b5e1ad536136687515b1ef4
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-03-20 11:35:17 +0530

    Update sigma.py

--- SiestaRobot/modules/sigma.py
+++ SiestaRobot/modules/sigma.py
@@ -1,4 +1,4 @@
-def sigma(update: SiestaRobot, context: CallbackContext):
+def sigma(update: Update, context: CallbackContext):
     update.effective_message.reply_video(random.choice(truth_and_dare_string.SIGMA))
 
 SIGMA = (

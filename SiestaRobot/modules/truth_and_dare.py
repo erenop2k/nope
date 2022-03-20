e86b449b4ad41e10427080b26900c05ad4564de4
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-03-20 18:44:53 +0530

    Update truth_and_dare.py

--- SiestaRobot/modules/truth_and_dare.py
+++ SiestaRobot/modules/truth_and_dare.py
@@ -18,45 +18,14 @@ def dare(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
 def sigma(update: Update, context: CallbackContext):
     update.effective_message.reply_video(random.choice(truth_and_dare_string.SIGMA))
-    
-    
-def gbam(update, context):
-    user = update.effective_user
-    chat = update.effective_chat
-    bot, args = context.bot, context.args
-    message = update.effective_message
-
-    curr_user = html.escape(message.from_user.first_name)
-    user_id = extract_user(message, args)
-
- if user_id:     
-    gbam_user = bot.get_chat(user_id)
-    user1 = curr_user
-    user2 = html.escape(gbam_user.first_name)
-       
-
- else:
-      user1 = curr_user
-      user2 = bot.first_name
-
- if update.effective_message.chat.type == "private":
-        return
- if int(user.id) in DRAGONS or int(user.id) in DEMONS:
-        gbamm = fun.GBAM
-        reason = random.choice(fun.GBAM_REASON)
-        gbam = gbamm.format(user1=user1, user2=user2, chatid=chat.id, reason=reason)
-        context.bot.sendMessage(chat.id, gbam, parse_mode=ParseMode.HTML)
-
 
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
 SIGMA_HANDLER = DisableAbleCommandHandler("sigma", sigma, run_async=True)
-GBAM_HANDLER = DisableAbleCommandHandler("gbam", gbam, run_async=True)
 
 
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 dispatcher.add_handler(SIGMA_HANDLER)
-dispatcher.add_handler(GBAM_HANDLER)

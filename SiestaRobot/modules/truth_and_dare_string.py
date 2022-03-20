813c9785fc2543b27830c93dfe08959ccce27a26
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-03-20 18:03:26 +0530

    Update truth_and_dare_string.py

--- SiestaRobot/modules/truth_and_dare_string.py
+++ SiestaRobot/modules/truth_and_dare_string.py
@@ -100,3 +100,27 @@ SIGMA = (
     "https://te.legra.ph/file/1e38235febfe52cdd0490.mp4",
     "https://te.legra.ph/file/e27fd617cc4e52835cb76.mp4",
 )
+GBAM = "<b>Beginning Of Global Bam For {user2}</b>  \n \nChat Id : <code>{chatid}</code> \nReason : <i>{reason}</i> \nGBammed By {user1}"
+
+
+GBAM_REASON = (
+    "sasta noob",
+    "sasta waifu stealer",
+    "sasta white-het hekur",
+    "sasta white-het codur",
+    "sasta white-het vala chintu",
+    "sasta hexa hekur",
+    "sasta hexa playur",
+    "sasta tiktokur💃🏾",
+    "sasta membor of team 7",
+    "sasta selmon boi",
+    "sasta fri-fire player",
+    "sasta chhota bhim",
+    "sasta chhapri",
+    "sasta jony sin",
+    "sasta chhapri nibba",
+    "sasta nibba",
+    "sasti ria chokroborti",
+    "sasti nibbi",
+    "sasta camper",
+)

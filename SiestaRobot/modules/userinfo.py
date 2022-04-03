a8ea1002023ec2fb9ebd49395337a91a6b1dd991
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-04-03 19:13:03 +0530

    Update userinfo.py

--- SiestaRobot/modules/userinfo.py
+++ SiestaRobot/modules/userinfo.py
@@ -258,20 +258,20 @@ def info(update: Update, context: CallbackContext):
 
     text = (
         f"╔═━「<b> Appraisal results:</b> 」\n"
-        f"✪ ID: <code>{user.id}</code>\n"
-        f"✪ First Name: {html.escape(user.first_name)}"
+        f"♡ ID: <code>{user.id}</code>\n"
+        f"♡ First Name: {html.escape(user.first_name)}"
     )
 
     if user.last_name:
-        text += f"\n✪ Last Name: {html.escape(user.last_name)}"
+        text += f"\n♡ Last Name: {html.escape(user.last_name)}"
 
     if user.username:
-        text += f"\n✪ Username: @{html.escape(user.username)}"
+        text += f"\n♡ Username: @{html.escape(user.username)}"
 
-    text += f"\n✪ Userlink: {mention_html(user.id, 'link')}"
+    text += f"\n♡ Userlink: {mention_html(user.id, 'link')}"
 
     if chat.type != "private" and user_id != bot.id:
-        _stext = "\n✪ Presence: <code>{}</code>"
+        _stext = "\n♡ Presence: <code>{}</code>"
 
         afk_st = is_afk(user.id)
         if afk_st:

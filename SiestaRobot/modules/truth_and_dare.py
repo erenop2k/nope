a98f8927addc89065ab115b6906862431c220d45
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-04-08 10:54:15 +0530

    Update truth_and_dare.py

--- SiestaRobot/modules/truth_and_dare.py
+++ SiestaRobot/modules/truth_and_dare.py
@@ -26,14 +26,15 @@ def rather(update: Update, context: CallbackContext):
     context.args
     update.effective_message.reply_text(random.choice(truth_and_dare_string.RATHER))
 
-
+def waifus(update: Update, context: CallbackContext):
+    update.effective_message.reply_photo(random.choice(truth_and_dare_string.WAIFUS))
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
 SIGMA_HANDLER = DisableAbleCommandHandler("sigma", sigma, run_async=True)
 COSPLAY_HANDLER = DisableAbleCommandHandler("cosplay", cosplay, run_async=True)
 RATHER_HANDLER = DisableAbleCommandHandler("rather", rather, run_async=True)
-
+WAIFUS_HANDLER = DisableAbleCommandHandler("waifus", waifus, run_async=True)
 
 
 
@@ -43,3 +44,4 @@ dispatcher.add_handler(DARE_HANDLER)
 dispatcher.add_handler(SIGMA_HANDLER)
 dispatcher.add_handler(COSPLAY_HANDLER)
 dispatcher.add_handler(RATHER_HANDLER)
+dispatcher.add_handler(WAIFUS_HANDLEE)

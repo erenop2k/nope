32bb61cb0c8a1a10d0b8d33a3057783935db714e
Jin Woo <99459682+Yoriichi-Tsugikuni@users.noreply.github.com>
2022-04-10 12:54:19 +0530

    Fixed Error Handler

--- SiestaRobot/modules/error_handler.py
+++ SiestaRobot/modules/error_handler.py
@@ -6,22 +6,16 @@ import traceback
 
 import pretty_errors
 import requests
-from telegram import(
-    InlineKeyboardButton,
-    InlineKeyboardMarkup,
-    ParseMode,
-    Update,
-)
-from telegram.ext import CallbackContext
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import CallbackContext, CommandHandler
 
 from SiestaRobot import DEV_USERS, ERROR_LOGS, dispatcher
-from SiestaRobot.modules.helper_funcs.decorators import siestacmd
 
 pretty_errors.mono()
 
 
 class ErrorsDict(dict):
-    """A custom dict to store errors and their count"""
+    "A custom dict to store errors and their count"
 
     def __init__(self, *args, **kwargs):
         self.raw = []
@@ -47,86 +41,79 @@ errors = ErrorsDict()
 def error_callback(update: Update, context: CallbackContext):
     if not update:
         return
-    if context.error not in errors:
-        try:
-            stringio = io.StringIO()
-            pretty_errors.output_stderr = stringio
-            output = pretty_errors.excepthook(
-                type(context.error),
-                context.error,
-                context.error.__traceback__,
-            )
-            pretty_errors.output_stderr = sys.stderr
-            pretty_error = stringio.getvalue()
-            stringio.close()
-        except:
-            pretty_error = "Failed to create pretty error."
-        tb_list = traceback.format_exception(
-            None,
+    if context.error in errors:
+        return
+    try:
+        stringio = io.StringIO()
+        pretty_errors.output_stderr = stringio
+        output = pretty_errors.excepthook(
+            type(context.error),
             context.error,
             context.error.__traceback__,
         )
-        tb = "".join(tb_list)
-        pretty_message = (
-            "{}\n"
-            "-------------------------------------------------------------------------------\n"
-            "An exception was raised while handling an update\n"
-            "User: {}\n"
-            "Chat: {} {}\n"
-            "Callback data: {}\n"
-            "Message: {}\n\n"
-            "Full Traceback: {}"
-        ).format(
-            pretty_error,
-            update.effective_user.id,
-            update.effective_chat.title if update.effective_chat else "",
-            update.effective_chat.id if update.effective_chat else "",
-            update.callback_query.data if update.callback_query else "None",
-            update.effective_message.text if update.effective_message else "No message",
-            tb,
-        )
-        extension = "txt"
-        url = "https://spaceb.in/api/v1/documents/"
-        try:
-            response = requests.post(
-                url, data={"content": pretty_message, "extension": extension}
-            )
-        except Exception as e:
-            return {"error": str(e)}
-        response = response.json()
-        e = html.escape(f"{context.error}")
-        if not response:
-            with open("error.txt", "w+") as f:
-                f.write(pretty_message)
-            context.bot.send_document(
-                ERROR_LOGS,
-                open("error.txt", "rb"),
-                caption=f"#{context.error.identifier}\n<b>Darling, i have an Error :"
-                f"</b>\n<code>{e}</code>",
-                parse_mode="html",
-            )
-            return
-
-        url = f"https://spaceb.in/{response['payload']['id']}"
-        context.bot.send_message(
+        pretty_errors.output_stderr = sys.stderr
+        pretty_error = stringio.getvalue()
+        stringio.close()
+    except:
+        pretty_error = "Failed to create pretty error."
+    tb_list = traceback.format_exception(
+        None,
+        context.error,
+        context.error.__traceback__,
+    )
+    tb = "".join(tb_list)
+    pretty_message = (
+        "{}\n"
+        "-------------------------------------------------------------------------------\n"
+        "An exception was raised while handling an update\n"
+        "User: {}\n"
+        "Chat: {} {}\n"
+        "Callback data: {}\n"
+        "Message: {}\n\n"
+        "Full Traceback: {}"
+    ).format(
+        pretty_error,
+        update.effective_user.id,
+        update.effective_chat.title if update.effective_chat else "",
+        update.effective_chat.id if update.effective_chat else "",
+        update.callback_query.data if update.callback_query else "None",
+        update.effective_message.text if update.effective_message else "No message",
+        tb,
+    )
+    key = requests.post(
+        "https://hastebin.com/documents",
+        data=pretty_message.encode("UTF-8"),
+    ).json()
+    e = html.escape(f"{context.error}")
+    if not key.get("key"):
+        with open("error.txt", "w+") as f:
+            f.write(pretty_message)
+        context.bot.send_document(
             ERROR_LOGS,
-            text=f"#{context.error.identifier}\n<b>Darling, i have an Error :"
-            f"</b>\n<code>{e}</code>",
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("See The Error Darling!", url=url)]],
-            ),
-            parse_mode=ParseMode.HTML,
+            open("error.txt", "rb"),
+            caption=f"#{context.error.identifier}\n<b>An unknown error occured:</b>\n<code>{e}</code>",
+            parse_mode="html",
         )
+        return
+    key = key.get("key")
+    url = f"https://hastebin.com/{key}"
+    context.bot.send_message(
+        ERROR_LOGS,
+        text=f"#{context.error.identifier}\n<b>An unknown error occured:</b>\n<code>{e}</code>",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("HasteBin", url=url)]],
+        ),
+        parse_mode="html",
+    )
 
 
-@siestacmd(command="errors")
 def list_errors(update: Update, context: CallbackContext):
     if update.effective_user.id not in DEV_USERS:
         return
     e = dict(sorted(errors.items(), key=lambda item: item[1], reverse=True))
     msg = "<b>Errors List:</b>\n"
     for x, value in e.items():
-        msg += f"× <code>{x}:</code> <b>{value}</b> #{x.identifier}\n"
+        msg += f"• <code>{x}:</code> <b>{value}</b> #{x.identifier}\n"
     msg += f"{len(errors)} have occurred since startup."
     if len(msg) > 4096:
         with open("errors_msg.txt", "w+") as f:
@@ -142,3 +129,4 @@ def list_errors(update: Update, context: CallbackContext):
 
 
 dispatcher.add_error_handler(error_callback)
+dispatcher.add_handler(CommandHandler("errors", list_error))

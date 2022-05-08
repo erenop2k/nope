f8d00f1f99c6287fbd01e68e6f6bfc7391fee26c
Light-Sensei <99256366+Light-Sensei@users.noreply.github.com>
2022-05-08 19:58:08 +0530

    Update __init__.py

--- SiestaRobot/__init__.py
+++ SiestaRobot/__init__.py
@@ -236,7 +236,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from MikuXProBot.modules.helper_funcs.handlers import (CustomCommandHandler,
+from SiestaRobot.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 
